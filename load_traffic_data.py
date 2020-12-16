9e6316a565ae75b87566660c23f08e640469287e
WexxanBest <57976769+WexxanBest@users.noreply.github.com>
2020-12-16 18:34:28 +0700

    Add files via upload

---
+++ load_traffic_data.py
@@ -0,0 +1,138 @@
+import json, os
+
+
+def organize_traffic_data(traffic: str, global_header_length=24, packet_header_length=16, packet_headers=None, use_cache=True, write_cache=True):
+    print('\n<DATA ORGANIZING>')
+    if use_cache:
+        print('Checking if there is cached data...')
+        traffic_dict = load_from_file()
+        if traffic_dict and traffic_dict['traffic data'] == traffic:
+            print('Cache was found! So cache will be used')
+            print('</DATA ORGANIZING>\n')
+            return traffic_dict
+        else:
+            print('There is no cache data.')
+
+    if packet_headers is None:
+        packet_headers = default_packets_header
+
+    traffic_dict = {'traffic data': traffic, 'global header': traffic[global_header_length * 2:], 'packets': {}}
+
+    # delete global header
+    data = traffic[global_header_length * 2:]
+    print('Organizing traffic data...')
+    while data:
+        packet = {}
+        # Getting main headers
+        main_header = data[:packet_header_length * 2]
+        data = data[packet_header_length * 2:]
+        packet['Main Header'] = main_header
+
+        packet_length = int(main_header[-8:].rstrip('0'), 16) * 2
+        packet['Packet Length'] = packet_length
+
+        # Getting all headers length
+        headers_length = 0
+        for header in packet_headers:
+            headers_length += header[1] * 2
+        # Getting all headers
+        for header in packet_headers:
+            current_header_name = header[0]
+            current_header_length = header[1] * 2
+            packet[current_header_name] = data[:current_header_length]
+            data = data[current_header_length:]
+
+        # Getting packet data
+        packet_data = data[:packet_length - headers_length]
+        data = data[packet_length - headers_length:]
+        packet['Data'] = packet_data
+
+        traffic_dict['packets'][len(traffic_dict['packets'])] = packet
+
+    print('Organizing finished!')
+    if write_cache:
+        print('Saving traffic data...')
+        safe_to_file(traffic_dict)
+        print('Traffic data was saved!')
+    print('</DATA ORGANIZING>\n')
+    return traffic_dict
+
+
+def safe_to_file(organized_traffic):
+    with open('traffic.json', 'w') as file:
+        json.dump(organized_traffic, file)
+
+
+def load_from_file():
+    try:
+        with open('traffic.json', 'r') as file:
+            organized_traffic = json.load(file)
+        return organized_traffic
+    except IOError:
+        return False
+
+
+def get_packets_from_traffic_data(traffic_data):
+    if isinstance(traffic_data, str):
+        traffic_data = organize_traffic_data(traffic_data)
+
+    clean_packets = []
+    packets = traffic_data['packets']
+    for packet in packets:
+        clean_packets += [packets[packet]['Data']]
+
+    return clean_packets, traffic_data
+
+
+def clean_workspace():
+    delete = input('Очистить старые результаты, если такие есть (по умолчанию - да) [y/n]:').strip()
+    if delete not in ('No', 'n', 'N', '0') or delete in ('y', 'Y', 'Yes', ''):
+        to_delete = ['answer.txt', 'success.txt'] + [f'packets{i}.json' for i in range(10)]
+        for file in to_delete:
+            try:
+                os.remove(file)
+                print(file, 'был удален.')
+            except:
+                pass
+    print()
+
+
+def device_delegator(packets, organized_traffic, default_device_amount=2):
+    devices = input('Введи кол-во устройств, между которыми будут распределены вычисленя: ')
+    try:
+        devices = int(devices)
+    except:
+        devices = default_device_amount
+
+    print('\n<DEVICE DELEGATOR>')
+    delegated_packets = []
+    if len(packets) % devices == 0:
+        print(f'Будет {devices} устройства. По {len(packets)//devices} пакетов для каждого устройства.')
+        for i in range(0, len(packets), len(packets)//devices):
+            delegated_packets += [packets[i:i+len(packets)//devices]]
+    else:
+        print(f'Будет {devices} устройства. По {len(packets)//devices} пакетов для первых {devices - 1} устройств и '
+               f'{len(packets) - (len(packets)//devices)*devices} для последнего устройства')
+        for i in range(0, (len(packets)//devices)*devices, len(packets)//devices):
+            delegated_packets += [packets[i:i+len(packets)//devices]]
+        delegated_packets += [packets[-(len(packets) - (len(packets)//devices)*devices):]]
+
+    for device_index in range(devices):
+        with open(f'packets{device_index}.json', 'w') as file:
+            json.dump({'all packets': organized_traffic['packets'], 'packets': delegated_packets[device_index], 'phrase': phrase}, file)
+            print(f'packets{device_index}.json был успешно создан')
+
+    print('</DEVICE DELEGATOR>\n')
+
+
+if __name__ == '__main__':
+    default_packets_header = [['Ethernet Header', 14], ['IPv4 Header', 20], ['UDP Header', 8]]
+
+    clean_workspace()
+    phrase = input('Введи фразу: ').strip()
+    traffic = input('Введи трафик: ')
+
+    packets, organized_traffic = get_packets_from_traffic_data(traffic)
+    device_delegator(packets, organized_traffic)
+
+    print('Необходимо теперь запустить main.py!')
---
+++ main.py
@@ -0,0 +1,212 @@
+import base64
+import os
+from multiprocessing import Process, current_process
+import hashlib
+from datetime import datetime
+import winsound
+import json
+
+
+def thread_delegator(packets: list, default_threads=6):
+    try:
+        threads = int(input('Введи кол-во потоков (по умолчанию 6): '))
+    except:
+        threads = default_threads
+
+    print('\n<DELEGATOR>')
+    delegated_packets = []
+    if len(packets) % threads == 0:
+        print(f'Будет {threads} потоков. {len(packets)//threads} пакетов для каждого потока.')
+        for i in range(0, len(packets), len(packets)//threads):
+            delegated_packets += [packets[i:i+len(packets)//threads]]
+    else:
+        print(f'Будет {threads} потоков. {len(packets)//threads} пакетов для первых {threads - 1} потоков и '
+               f'{len(packets) - (len(packets)//threads)*threads} для последнего потока')
+        for i in range(0, (len(packets)//threads)*threads, len(packets)//threads):
+            delegated_packets += [packets[i:i+len(packets)//threads]]
+        delegated_packets += [packets[-(len(packets) - (len(packets)//threads)*threads):]]
+    print('</DELEGATOR>')
+    return {'threads': threads, 'packets': delegated_packets}
+
+
+def find_answer(all_packets):
+    try:
+        file = open('success.txt', 'r')
+        line = file.readline()
+        key = int(line.split()[0])
+    except:
+        return False
+
+    data = []
+    decr_data = []
+    packs = all_packets
+    mac_server = ''
+    for packet in packs:
+        dec = decode_text(packs[packet]['Data'], key)
+        if dec != None:
+            if dec.isdigit():
+                mac_server = packs[packet]["Ethernet Header"][0:12]
+                #print(mac_server)
+            if packs[packet]["Ethernet Header"][12:24] == mac_server:
+                data.append(dec)
+                decr_data.append(packs[packet])
+    #print(data)
+    answer = hashlib.sha256("\n".join(data).encode()).hexdigest()
+    with open('answer.txt', 'w') as file:
+        file.write(str(answer))
+
+    return answer
+
+
+def decode_text(message: str, secret_key, add_symbols=False, m=2038074743):
+    # print = DebugPrint(debugging).debug_print
+    # print('\n<Decoding>')
+    result = ''
+    for i in range(0, len(message), 8):
+        # print(message[i:i+8])
+        # print(str(hex((int(message[i:i+8], 16) - int(hex(secret_key), 16)) % int(hex(m), 16))))
+        result += str(hex((int(message[i:i+8], 16) - int(hex(secret_key), 16)) % int(hex(m), 16)))[2:]
+
+    # print('Decoding steps:')
+    if len(result) % 8 and add_symbols:
+        result += '0' * (8 - len(result) % 8)
+    # print('0)', message)
+    try:
+        # print('1)', end=' ')
+        #for i in range(0, len(result), 8):
+            # print(result[i:i + 8], end=' ')
+        # print('')
+
+        result = bytes.fromhex(result)
+        # print('2)', result)
+
+        result = base64.b64decode(result)
+        result = result.decode()
+
+        # print('3)', result)
+
+    except UnicodeDecodeError as er:
+        # print('<!> UnicodeDecodeError occurred! <!>', er)
+        result = None
+    except Exception as ex:
+        # print('<!> Error occurred!', ex)
+        result = None
+    # print('-'*20)
+    # print('Key:', secret_key)
+    # print(f'Original message: {"0x" + message} (len: {len(message)})\nDecoded message: {result}')
+    # if result:
+    #     # print('</Decoding> (SUCCESS)\n')
+    # else:
+    #     # print('</Decoding> (FAIL)\n')
+    return result
+
+
+def check_answer():
+    try:
+        file = open('success.txt', 'r')
+        file.close()
+        return True
+    except IOError:
+        return False
+
+
+def define_key(packets, phrase):
+    key_progress = [4290772992, 4290814935, 4290856878, 4290898821, 4290940764, 4290982707, 4291024650, 4291066593,
+                    4291108536, 4291150479, 4291192422, 4291234365, 4291276308, 4291318251, 4291360194, 4291402137,
+                    4291444080, 4291486023, 4291527966, 4291569909, 4291611852, 4291653795, 4291695738, 4291737681,
+                    4291779624, 4291821567, 4291863510, 4291905453, 4291947396, 4291989339, 4292031282, 4292073225,
+                    4292115168, 4292157111, 4292199054, 4292240997, 4292282940, 4292324883, 4292366826, 4292408769,
+                    4292450712, 4292492655, 4292534598, 4292576541, 4292618484, 4292660427, 4292702370, 4292744313,
+                    4292786256, 4292828199, 4292870142, 4292912085, 4292954028, 4292995971, 4293037914, 4293079857,
+                    4293121800, 4293163743, 4293205686, 4293247629, 4293289572, 4293331515, 4293373458, 4293415401,
+                    4293457344, 4293499287, 4293541230, 4293583173, 4293625116, 4293667059, 4293709002, 4293750945,
+                    4293792888, 4293834831, 4293876774, 4293918717, 4293960660, 4294002603, 4294044546, 4294086489,
+                    4294128432, 4294170375, 4294212318, 4294254261, 4294296204, 4294338147, 4294380090, 4294422033,
+                    4294463976, 4294505919, 4294547862, 4294589805, 4294631748, 4294673691, 4294715634, 4294757577,
+                    4294799520, 4294841463, 4294883406, 4294925349, 4294967296]
+
+    possible_keys = list(range((2 ** 32 - 2 ** 22), 2 ** 32 + 1))
+
+    def write_key(key, packet, result):
+        with open('success.txt', 'w') as file:
+            file.write(f'{key} {packet} {result}')
+
+    for key in possible_keys:
+        if key in key_progress:
+            key_progress_index = key_progress.index(key)
+            print(current_process().name, f'перебрал {key_progress_index * 41943} ключей ({key_progress_index} %). Текущий: {key}')
+        if check_answer():
+            return
+        for i, packet in enumerate(packets):
+            res = decode_text(packet, key)
+            if res and phrase in res:
+                print('\nNice!!!!\n')
+                write_key(key, packet, res)
+                return
+
+
+def start_multiprocessing(function, deligator_data: dict, phrase, extra_function_args=(), extra_info=''):
+    if extra_info:
+        print(f'\n<MULTIPROCESSING> ({extra_info})')
+    else:
+        print('\n<MULTIPROCESSING>')
+
+    threads = deligator_data['threads']
+    packets = deligator_data['packets']
+
+    print('Запускаю потоки...')
+    processes = []
+    for i in range(threads):
+        process = Process(target=function, args=(packets[i], phrase) + extra_function_args, name=f'Поток {i}')
+        processes.append(process)
+        process.start()
+        print(f'Поток {i} запущен.')
+
+   # print(processes)
+
+    for i, proc in enumerate(processes):
+        proc.join()
+        print(f'Поток {i} завершен')
+
+    print('</MULTIPROCESSING>')
+
+
+def load_data():
+    for i in range(10):
+        try:
+            file = open(f'packets{i}.json', 'r')
+            print(f'packets{i}.json был открыт!')
+            break
+        except:
+            pass
+    data, all_packets, packets, phrase = {}, {}, {}, ''
+    try:
+        data = json.load(file)
+        all_packets = data['all packets']
+        packets = data['packets']
+        phrase = data['phrase']
+    except:
+        print('Данных нет! Сначала загрузи данные в load_traffic_data.py')
+        quit()
+    return packets, all_packets, phrase
+
+
+if __name__ == '__main__':
+    possible_keys = list(range((2 ** 32 - 2 ** 22), 2 ** 32 + 1))
+    key_progress = [4290772992, 4290814935, 4290856878, 4290898821, 4290940764, 4290982707, 4291024650, 4291066593, 4291108536, 4291150479, 4291192422, 4291234365, 4291276308, 4291318251, 4291360194, 4291402137, 4291444080, 4291486023, 4291527966, 4291569909, 4291611852, 4291653795, 4291695738, 4291737681, 4291779624, 4291821567, 4291863510, 4291905453, 4291947396, 4291989339, 4292031282, 4292073225, 4292115168, 4292157111, 4292199054, 4292240997, 4292282940, 4292324883, 4292366826, 4292408769, 4292450712, 4292492655, 4292534598, 4292576541, 4292618484, 4292660427, 4292702370, 4292744313, 4292786256, 4292828199, 4292870142, 4292912085, 4292954028, 4292995971, 4293037914, 4293079857, 4293121800, 4293163743, 4293205686, 4293247629, 4293289572, 4293331515, 4293373458, 4293415401, 4293457344, 4293499287, 4293541230, 4293583173, 4293625116, 4293667059, 4293709002, 4293750945, 4293792888, 4293834831, 4293876774, 4293918717, 4293960660, 4294002603, 4294044546, 4294086489, 4294128432, 4294170375, 4294212318, 4294254261, 4294296204, 4294338147, 4294380090, 4294422033, 4294463976, 4294505919, 4294547862, 4294589805, 4294631748, 4294673691, 4294715634, 4294757577, 4294799520, 4294841463, 4294883406, 4294925349, 4294967296]
+
+    packets, all_packets, phrase = load_data()
+    thread_delegator_data = thread_delegator(packets)
+
+    start_time = datetime.now()
+    start_multiprocessing(define_key, thread_delegator_data, phrase, extra_info=f'Начало в {datetime.now()}')
+    print('Поиск занял:', datetime.now() - start_time)
+
+    answer = find_answer(all_packets)
+    if answer:
+        print(answer)
+    else:
+        print('Ответ не был найден')
+
+    winsound.MessageBeep()
+    os.system('pause')
