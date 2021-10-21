383f62956f307b9b7539d60b67be18613d720bd0
Wexxan <57976769+WexxanBest@users.noreply.github.com>
2021-10-22 00:44:11 +0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+import webbrowser
+
+
+links = ['https://www.xvideos.com/video65892799/brand_new_learning_to_use_twitter_with_25cm_in_the_ass',
+         'https://www.xvideos.com/video65470401/machos_fucking_on_twitter_14_-_best_gay_ebooks_at_https_payhip.com_allanmoraes',
+         'https://www.xvideos.com/video65470523/machos_fucking_on_twitter_17_-_best_gay_ebooks_at_https_payhip.com_allanmoraes',
+         'https://www.xvideos.com/video10078504/gay_cumpilation_-_hot_cum_shots',
+         'https://www.xvideos.com/video65546147/machos_fucking_on_twitter_27_-_best_gay_ebooks_at_https_payhip.com_allanmoraes',
+         'https://www.xvideos.com/video57663383/teen_lad_caught_in_the_woods_got_tied_up_a._and_to_cum',
+         'https://www.xvideos.com/video34986549/daddie_hard_fuck',
+         'https://www.xvideos.com/video47591381/gym_dudes_have_a_raw_threesome._fat_ass_dude_gets_used_by_2_studs_met_at_the_gym._twitter_thebaddestbunny_ig_baddestbunnyy',
+         'https://www.xvideos.com/video63632899/straight_contruction_worker_gets_tricked_into_gay_sex',
+         'https://www.xvideos.com/video64549657/the_hairdresser',
+         'https://www.xvideos.com/video41754299/young_twink_step_brother_needs_help_from_his_older_step_brother_with_sex_dreams_pov']
+
+
+for link in links:
+    webbrowser.open_new_tab(link)
